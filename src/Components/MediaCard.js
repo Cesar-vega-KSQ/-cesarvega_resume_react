0914fd35b7a56b11f74f4449a39451d8f230a2fe
Cesar-vega-KSQ <cesar.vega@theksquaregroup.com>
2020-10-30 22:01:45 -0600

    a

--- src/Components/MediaCard.js
+++ src/Components/MediaCard.js
@@ -4,6 +4,8 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import LazyLoad from 'react-lazyload';
+
 
 const useStyles = makeStyles({
   root: {
